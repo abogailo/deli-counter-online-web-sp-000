cd9f53ae9f8c24ce6da45fec682cc72b4415d9a4
Abagail Hudson <abagailanne@gmail.com>
2019-05-09 01:34:34 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,12 +1,11 @@
 # Write your code here.
 katz_deli = []
 
- def line(katz_deli)
+def line(katz_deli)
   if katz_deli.empty?
     puts "The line is currently empty."
-  else
-    puts "The line is currently: 1. #{katz_deli[0]} 2. #{katz_deli[1]} 3. #{katz_deli[2]}"
-  end
+    katz_deli.each_with_index  {|name, index| line.push("#{index +1 }.", name)}
+    puts "The line is currently: #{line.join(" ")}"	
 end
 
  def take_a_number(katz_deli,person)
