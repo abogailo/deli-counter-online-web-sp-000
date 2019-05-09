724a6a591dc246f6b2c6849bc07193922cac190b
Abagail Hudson <abagailanne@gmail.com>
2019-05-09 01:41:38 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,30 +1,22 @@
 # Write your code here.
-katz_deli = []
 
-def line(katz_deli)
-  if katz_deli.empty?
-    puts "The line is currently empty."
-  elsif !katz_deli.empty?
-    katz_deli.each_with_index  {|name, index| line.push("#{index +1 }.", name)}
-    puts "The line is currently: #{line.join(" ")}"
-  end
+def line(katz_deli)	def line(katz_deli)
+if katz_deli.count == 0	if katz_deli.count == 0
+    puts "The line is currently empty."	    puts "The line is currently empty."
+@@ -16,6 +8,15 @@ def line(katz_deli)
+  katz_deli.each_with_index  {|name, index| line.push("#{index +1 }.", name)}	  katz_deli.each_with_index  {|name, index| line.push("#{index +1 }.", name)}
+    puts "The line is currently: #{line.join(" ")}"	    puts "The line is currently: #{line.join(" ")}"
+end	end
+
+ def take_a_number(katz_deli, name)
+  katz_deli.push(name)
+  position = katz_deli.index(name)
+  puts"Welcome, #{name}. You are number #{katz_deli.index(name)+1} in line."
+
+ return name, position
 end
 
- def take_a_number(katz_deli,person)
-  if katz_deli.empty?
-    katz_deli.push (person)
-    puts "Welcome, #{person}. You are number 1 in line."
-  else
-    katz_deli.push (person)
-    puts "Welcome, #{person}. You are number 4 in line."
-  end
-end
+ end	end
 
- def now_serving(katz_deli)
-  if katz_deli.empty?
-    puts "There is nobody waiting to be served!"
-  else
-    puts "Currently serving #{katz_deli[0]}."
-    katz_deli.shift
-  end
-end
+
+ def now_serving (katz_deli)	def now_serving (katz_deli)
