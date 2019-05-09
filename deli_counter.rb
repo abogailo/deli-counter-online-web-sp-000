177a1920f0be369ca0be162ad57d81ebda7cfbdb
Abagail Hudson <abagailanne@gmail.com>
2019-05-09 01:46:20 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,30 +1,32 @@
 # Write your code here.
 
 
-def line(katz_deli)
-  if katz_deli.empty?
-    puts "The line is currently empty."
-  end
-    katz_deli.each_with_index  {|name, index| line.push("#{index +1 }.", name)}
-    puts "The line is currently: #{line.join(" ")}"
-
+def take_a_number(katz_deli, name)
+  katz_deli << name
+  puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
 end
 
- def take_a_number(katz_deli,person)
-  if katz_deli.empty?
-    katz_deli.push (person)
-    puts "Welcome, #{person}. You are number 1 in line."
-  else
-    katz_deli.push (person)
-    puts "Welcome, #{person}. You are number 4 in line."
-  end
-end
-
- def now_serving(katz_deli)
-  if katz_deli.empty?
+def now_serving(katz_deli)
+  if katz_deli.length == 0
     puts "There is nobody waiting to be served!"
   else
-    puts "Currently serving #{katz_deli[0]}."
-    katz_deli.shift
+    puts "Currently serving #{katz_deli.shift}."
   end
+
+end
+
+def line(katz_deli)
+  if katz_deli.length == 0
+    puts "The line is currently empty."
+  else
+    message="The line is currently:"
+
+    katz_deli.each_with_index do |value, index|
+      message += " #{index.to_i+1}. #{value}"
+    end
+
+    puts "#{message}"
+  end
+
+
 end
